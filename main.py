c7e8bdf635e04b9dd53adf89256e2d6ea33f9f33
eleven <274563150@qq.com>
2019-01-13 23:51:25 +0800

    update

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import numpy as np
 from model import  textCNN_Kim
 from torch.utils.data import TensorDataset
 import random
+import os
+from torch.autograd import Variable as V
 
 MODEL_PATH = "models/sgns.target.word-ngram.1-2.dynwin5.thr10.neg5.dim300.iter5"
 POS_PATH = "seg_pos.txt"
@@ -38,6 +40,7 @@ def load_data(path,label):
             # i += 1
             # if i < 30:
                 #print(line)
+            line = line[1:-1].strip()
             sentenc_emb = line_to_vector(line)
             #print(sentenc_emb)
             total_emb.append(sentenc_emb)
@@ -73,41 +76,54 @@ train_data = data[:la]
 batch_size = 3
 train_data_x_batch = []
 train_data_y_batch = []
-
 #train_data_x =[train_data for i,train_data in enumerate(train_data)]
-
-
 train_data_x = []
 train_data_y = []
 for i,(x,y) in enumerate(train_data):
     train_data_x.append(x)
     train_data_y.append(y)
+    # print("i:{}".format(i))
+    # print(len(train_data_x))
     if i != 0 and i % batch_size == 0:
         train_max_len = max((len(l) for l in train_data_x))
-        train_data_x = [np.vstack((l, np.zeros(((train_max_len - len(l)), 300)))) for l in train_data_x]
-        train_data_x_batch.append(train_data_x)
+        # print(train_max_len)
+        # print(len(train_data_x))
+        # print(len(train_data_x[0]))
+        train_data_x1 = [np.vstack((l, np.zeros(((train_max_len - len(l)), 300)))) for l in train_data_x]
+        #print("--::::::::")
+        train_data_x_batch.append(train_data_x1)
         train_data_y_batch.append(train_data_y)
         train_data_x = []
         train_data_y = []
 print("train data size:{}".format(len(train_data_x_batch)))
 
-train_data_x_b = torch.FloatTensor(train_data_x_batch)
-train_data_y_b = torch.LongTensor(train_data_y_batch)
+# train_data_x_b = torch.FloatTensor(train_data_x_batch)
+# train_data_y_b = torch.LongTensor(train_data_y_batch)
+batch_data = list(zip(train_data_x_batch,train_data_y_batch))
+
 #train_data_x = torch.FloatTensor(train_data_x)
 print("-----")
 
-# test_data = data[la:]
-# test_data_x = []
-# test_data_y = []
-# for i,(x,y) in enumerate(test_data):
-#     test_data_x.append(x)
-#     test_data_y.append(y)
-# print("test data size:{}".format(len(test_data_x)))
-# test_max_len = max((len(l) for l in test_data_x))
-# print("test_max_len：{}".format(test_max_len))
-# #test_data_x = np.array([np.vstack((l, np.zeros(((test_max_len - len(l)), 300)))) for l in test_data_x]).astype(np.float64)
-# test_data_x = torch.FloatTensor(test_data_x)
-# test_data_y = torch.LongTensor(test_data_y)
+test_data = data[la:]
+test_data_x_batch = []
+test_data_y_batch = []
+test_data_x = []
+test_data_y = []
+for i,(x,y) in enumerate(test_data):
+    test_data_x.append(x)
+    test_data_y.append(y)
+    if i != 0 and i % batch_size == 0:
+        test_max_len = max((len(l) for l in test_data_x))
+        test_data_x1 = [np.vstack((l, np.zeros(((test_max_len - len(l)), 300)))) for l in test_data_x]
+        test_data_x_batch.append(test_data_x1)
+        test_data_y_batch.append(test_data_y)
+        test_data_x = []
+        test_data_y = []
+
+print("test data size:{}".format(len(test_data_x_batch)))
+#test_data_x = np.array([np.vstack((l, np.zeros(((test_max_len - len(l)), 300)))) for l in test_data_x]).astype(np.float64)
+# test_data_x = torch.FloatTensor(test_data_x_batch)
+# test_data_y = torch.LongTensor(test_data_y_batch)
 
 
 net = textCNN_Kim(1,300,2,100,[2,3,4])
@@ -123,32 +139,39 @@ loss_function = torch.nn.CrossEntropyLoss()
 
 EPOCH = 1
 for epoch in range(EPOCH):
-    for index,item in train_data_x_b:
-        x = item.cuda()
-        y = train_data_y_b[index].cuda
+    random.shuffle(batch_data)
+    #for batch,(x,y) in batch_data:
+    for batch,item in enumerate(train_data_x_batch):
+        x = item
+        y = train_data_y_batch[batch]
+        x = torch.FloatTensor(x)
+        y = torch.LongTensor(y)
+        x = V(x.cuda())
+        # x.cuda()
+        # y.cuda()
+        out = net(x)
+        # print(out)
+        # print(out.size())
+        loss = loss_function(out,V(y.cuda()))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch % 2 == 0:
+            net.eval()
+            print("step:---")
+            accuracy = 0
+            for index,item in enumerate(test_data_x_batch):
+                test_x = item
+                test_y = test_data_y_batch[index]
+                test_x = torch.FloatTensor(test_x).cuda()
+                #test_y = torch.LongTensor(test_y).cuda()
 
-
-    # for step,(x,y) in enumerate(train_loader):
-    #     x.cuda()
-    #     y.cuda()
-    #     out = net(x)
-    #     # print(out)
-    #     # print(out.size())
-    #     loss = loss_function(out,y).cuda()
-    #     optimizer.zero_grad()
-    #     loss.backward()
-    #     optimizer.step()
-    #     if step % 2 == 0:
-    #         net.eval()
-    #         predict = net(test_data_x.cuda())
-    #         #print(predict)
-    #         pred_y = torch.max(predict,1)[1].data.numpy()
-    #         accuracy = float((pred_y==test_data_y.data.numpy()).astype(int).sum())/float(len(test_data_y))
-    #         print("epoch:%f,step:%f,loss:%.4f,accuracy:%.4f" % (epoch,step,loss.data.numpy(),accuracy))
-    #         net.train()
-
-
-
-
+                predict = net(test_x.cuda())
+                #print(predict)
+                pred_y = torch.max(predict,1)[1].data.cpu().numpy()
+                accuracy += float((pred_y==test_y).astype(int).sum())
+            accuracy = accuracy/len(test_data)
+            print("epoch:%f,step:%f,loss:%.4f,accuracy:%.4f" % (epoch,batch,loss.data.numpy(),accuracy))
+            net.train()
 
 
